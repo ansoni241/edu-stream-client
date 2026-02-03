0d4930036ba7c0d366c6056c3408c25b3dffa14b
Anthony Gomez <anthonyansoni24@gmail.com>
2026-02-02 22:48:43 -0400

    Configuracion basica del Frontend

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Detail from './components/pages/Detail'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import Courses from './components/pages/Courses'
+import MyCourses from './components/pages/account/MyCourses'
+import MyLearning from './components/pages/account/MyLearning'
+import WatchCourse from './components/pages/account/WatchCourse'
+import ChangePassword from './components/pages/account/ChangePassword'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/account/login" element={<Login />} />
+          <Route path="/account/register" element={<Register />} />
+          <Route path="/account/my-courses" element={<MyCourses />} />
+          <Route path="/account/courses-enrolled" element={<MyLearning/>} />
+          <Route path="/account/watch-course" element={<WatchCourse/>} />
+          <Route path="/account/change-password" element={<ChangePassword/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
