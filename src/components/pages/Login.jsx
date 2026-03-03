c8ee5eb2bc0988f06aa02bb56fb335a2ae38109d
Anthony Gomez <anthonyansoni24@gmail.com>
2026-03-03 11:01:06 -0400

    feat: implementar vistas de login y registro

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,28 +1,84 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Layout from '../common/Layout'
+import { useForm } from 'react-hook-form';
+import { API_URL } from '../common/Config';
+import toast from 'react-hot-toast';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const {
+    handleSubmit, register, formState: { errors }, setError
+  } = useForm();
+  const onSubmit = async (data) => {
+    await fetch(`${API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(result => {
+        if (result.status == 200) {
+          const userInfo = {
+            name: result.name,
+            id: result.id,
+            token: result.token,
+          }
+          localStorage.setItem('userInfo', JSON.stringify(userInfo));
+          navigate('/account/dashboard');
+        } else {
+          toast.error(result.message);
+        }
+      })
+  }
   return (
     <Layout>
       <div className='container py-5 mt-5'>
         <div className='d-flex align-items-center justify-content-center'>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className='card border-0 shadow login'>
               <div className='card-body p-4'>
                 <h3 className='border-bottom pb-3 mb-3'>Login</h3>
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="email">Email</label>
                   <input
-                    type="text" className='form-control' placeholder='Email' />
+                    {
+                      ...register("email", {
+                        required: "El email es obligatorio.",
+                        pattern: {
+                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          message: "Dirección de email no válida."
+                        }
+                      })
+                    }
+                    type="text"
+                    className={`form-control ${errors.email && 'is-invalid'}`}
+                    placeholder='Email'
+                  />
+                  {errors.email && <div className='invalid-feedback'>{errors.email.message}</div>}
                 </div>
 
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="password">Password</label>
                   <input
-                    type="password" className='form-control'
-                    placeholder='Password' />
-
+                    {
+                      ...register("password", {
+                        required: "La contraseña es obligatoria.",
+                      })
+                    }
+                    type="password"
+                    className={`form-control ${errors.password && 'is-invalid'}`}
+                    placeholder='Password'/>
+                  {
+                    errors.password && (
+                      <div className='invalid-feedback'>
+                        {errors.password.message}
+                      </div>
+                    )
+                  }
                 </div>
 
                 <div className='d-flex justify-content-between align-items-center'>
--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Layout from '../common/Layout'
+import { useForm } from 'react-hook-form';
+import { API_URL } from '../common/Config';
+import toast from 'react-hot-toast';
 
 const Register = () => {
+  const navigate = useNavigate();
+  const {
+    handleSubmit, register, formState: { errors }, setError
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    await fetch(`${API_URL}/register`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(result => {
+      console.log(result);
+      if(result.status == 200){
+        toast.success(result.message);
+        navigate('/account/login');
+      }else {
+        const errors = result.errors;
+        Object.keys(errors).forEach(field => {
+          setError(field, {
+            message: errors[field][0]
+          });
+        });
+      }
+    })
+  }
+
   return (
     <Layout>
       <div className='container py-5 mt-5'>
         <div className='d-flex align-items-center justify-content-center'>
-          <form >
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className='card border-0 shadow register'>
               <div className='card-body p-4'>
                 <h3 className='border-bottom pb-3 mb-3'>Register</h3>
@@ -15,26 +49,69 @@ const Register = () => {
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="name">Name</label>
                   <input
+                    {
+                      ...register("name", {
+                        required: "El nombre es obligatorio."
+                      })
+                    }
                     type="text"
-                    className={`form-control`}
+                    className={`form-control ${errors.name && 'is-invalid'}`}
                     placeholder='Name' />
+                    {
+                      errors.name && (
+                        <div className='invalid-feedback'>
+                          {errors.name.message}
+                        </div>
+                      )
+                    }
                 </div>
 
 
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="email">Email</label>
                   <input
-                    type="text" className={`form-control`}
+                    {
+                      ...register("email", {
+                        required: "El email es obligatorio.",
+                        pattern: {
+                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          message: "El email no es válido"
+                        }
+                      })
+                    }
+                    type="text" className={`form-control ${errors.email && 'is-invalid'}`}
                     placeholder='Email' />
-
+                    {                      
+                      errors.email && (
+                        <div className='invalid-feedback'>
+                          {errors.email.message}
+                        </div>
+                      )
+                    }
                 </div>
 
                 <div className='mb-3'>
                   <label className='form-label' htmlFor="password">Password</label>
                   <input
+                    {
+                      ...register("password", {
+                        required: "El password es obligatorio.",
+                        minLength: {
+                          value: 8,
+                          message: "El password debe tener al menos 8 caracteres"
+                        }
+                      })
+                    }
                     type="password"
-                    className={`form-control`}
+                    className={`form-control ${errors.password && 'is-invalid'}`}
                     placeholder='Password' />
+                    {
+                      errors.password && (
+                        <div className='invalid-feedback'>
+                          {errors.password.message}
+                        </div>
+                      )
+                    }
                 </div>
 
                 <div>
